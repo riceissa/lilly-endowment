c3428d1cedf7b8bad48bc4ef5bc17d1fa1427b04
Issa Rice <riceissa@gmail.com>
2018-08-06 10:22:56 -0700

    print the SQL

--- proc.py
+++ proc.py
@@ -4,6 +4,18 @@ import sys
 from bs4 import BeautifulSoup
 
 
+def mysql_quote(x):
+    """Quote the string x using MySQL quoting rules. If x is the empty string,
+    return "NULL". Probably not safe against maliciously formed strings, but
+    our input is fixed and from a basically trustable source."""
+    if not x:
+        return "NULL"
+    x = x.replace("\\", "\\\\")
+    x = x.replace("'", "''")
+    x = x.replace("\n", "\\n")
+    return "'{}'".format(x)
+
+
 def main():
     for filepath in sys.argv[1:]:
         with open(filepath, "r") as f:
@@ -30,16 +42,43 @@ def soup_to_grants_generator(soup, focus_area, month):
             "community-development": "Community development",
             "education-youth": "Education/Youth",
             "religion": "Religion",
-            }
+        }
         yield {"grantee": grantee, "grantee_location": grantee_location,
                "amount": amount, "purpose": purpose,
                "focus_area": focus_area_map[focus_area],
-               "date": month + "-01"}
+               "date": month + "-01", "url": grant_url(focus_area, month)}
+
+
+def grant_url(focus_area, month):
+    """Use the focus area and donation date information to get the original grant URL."""
+    fa = {
+        "community-development": 13,
+        "education-youth": 28,
+        "religion": 11,
+    }[focus_area]
+    month_param = month[2:]
+
+    return f"https://lillyendowment.org/for-current-grantees/recent-grants/?fa={fa}&date={month_param}"
 
 
 def print_sql(grants_generator):
+    print("""insert into donations (donor, donee, amount, donation_date, donation_date_precision donation_date_basis, cause_area, url, donor_cause_area_url, notes, affected_cities) values""")
+    first = True
     for grant in grants_generator:
-        print(grant)
+        print(("    " if first else "    ,") + "(" + ",".join([
+            mysql_quote("Lilly Endowment"),  # donor
+            mysql_quote(grant["grantee"]),  # donee
+            str(grant["amount"]),  # amount
+            mysql_quote(grant["date"]),  # donation_date
+            mysql_quote("month"),  # donation_date_precision
+            mysql_quote("donation log"),  # donation_date_basis
+            mysql_quote(grant["focus_area"]),  # cause_area
+            mysql_quote(grant["url"]),  # url
+            mysql_quote("Purpose: " + grant["purpose"]),  # notes
+            mysql_quote(grant["grantee_location"]),  # affected_cities
+        ]) + ")")
+        first = False
+    print(";")
 
 if __name__ == "__main__":
     main()
